b36177fa85b2aa5117a5da9b2943a47c4bcd8335
Grail-Lin <98571964+Grail-Lin@users.noreply.github.com>
2023-08-10 17:13:33 +0800

    file for pd reader and qrcode reader

---
+++ backup/qrcode_reader_test.py
@@ -0,0 +1,96 @@
+'''
+# command format: read_pd.py <pwn=0-1> <time=10>
+from argparse import ArgumentParser
+parser = ArgumentParser()
+# add static parameters
+parser.add_argument('voltage', type=float, help='led voltage power, [0-5]')
+parser.add_argument('time', type=int, help='time for led period')
+args = parser.parse_args()
+'''
+'''
+initial: 
+send 57 00 00 03 04 01 00 00 00 00 00 1F 71 50 41
+recv 31 00 00 03 04 01 00 00 00 00 00 FF F8 50 41
+
+send 57 00 00 03 04 03 00 00 00 04 00 00 00 00 00 F7 81 50 41
+recv 31 00 00 03 04 03 00 00 00 00 00 FE 1A 50 41
+
+
+stop decoding:
+send 57 00 00 03 04 03 00 00 00 04 00 01 00 00 00 F6 7D 50 41
+
+Aztec Code Test = 
+41 7A 74 65 63 20 43 6F 64 65 20 54 65 73 74 0D
+'''
+
+'''
+launch program
+
+inital qr code reader
+	send 57 00 00 03 04 01 00 00 00 00 00 1F 71 50 41
+    recv 31 00 00 03 04 01 00 00 00 00 00 FF F8 50 41
+
+if error, exit
+
+if ok, do while loop for trigger
+
+'''
+
+import time
+import serial
+
+PORT = 'COM10'
+
+ser = serial.Serial(PORT, 115200, timeout=0.050)
+
+count = 0
+state = 0
+
+init_package = bytearray(b'\x57\x00\x00\x03\x04\x01\x00\x00\x00\x00\x00\x1F\x71\x50\x41')
+
+init_OK_package = bytearray(b'\x31\x00\x00\x03\x04\x01\x00\x00\x00\x00\x00\xFF\xF8\x50\x41')
+
+scan_package = bytearray(b'\x57\x00\x00\x03\x04\x03\x00\x00\x00\x04\x00\x00\x00\x00\x00\xF7\x81\x50\x41')
+
+scan_OK_package = bytearray(b'\x31\x00\x00\x03\x04\x03\x00\x00\x00\x00\x00\xFE\x1A\x50\x41')
+
+stop_package = bytearray(b'\x57\x00\x00\x03\x04\x03\x00\x00\x00\x04\x00\x01\x00\x00\x00\xF6\x7D\x50\x41')
+
+
+ser.write(init_package)
+
+while 1:
+    if(state == 0):          //waits for incoming data
+        while ser.in_waiting:
+            data = ser.readline().decode("ascii")
+            if(data == '1')  //received a '1' move onto next state
+                state = 1;
+                print("1 received")
+                ser.write("1 received").encode()
+            else:           //wrong data stay at state 0
+                print("back to the start")
+                ser.write("back to the start").encode()
+
+    elif(state == 1):          //waits for incoming data
+        while ser.in_waiting:
+            data = ser.readline().decode("ascii")
+            if(data == '2')  //received a '2' move on to next state
+                state = 2;
+                print("2 received")
+                ser.write("2 received").encode()
+            else:            //wrong data return to state 0  
+                state = 0;
+                print("back to the start")
+                ser.write("back to the start").encode()
+
+    elif(state == 2):          //waits for incoming data
+        while ser.in_waiting:
+            data = ser.readline().decode("ascii")
+            if(data == '3')  //received a '3'  //received a '3' print message
+                print("You win!")
+                ser.write("You win!").encode()
+                state = 0; 
+            else:             //wrong data return to state 0  
+                state = 0;
+                print("back to the start")
+                ser.write("back to the start").encode()
---
+++ backup/read_pd.py
@@ -0,0 +1,112 @@
+
+# command format: read_pd.py <pwn=0-1> <time=10>
+from argparse import ArgumentParser
+parser = ArgumentParser()
+# add static parameters
+parser.add_argument('voltage', type=float, help='led voltage power, [0-5]')
+parser.add_argument('time', type=int, help='time for led period')
+args = parser.parse_args()
+
+'''
+triger led voltage = pwn for time = time
+read analog input from pin
+'''
+
+
+
+from pyfirmata2 import Arduino, PWM
+import time
+import math
+
+#PORT = 'COM3'
+PORT = Arduino.AUTODETECT
+
+
+class PDReader:
+    '''
+        triger led voltage = pwm for time = time
+        read analog input from pin
+    '''
+    def __init__(self, pwm=1):
+        
+        # output file
+        #self.h_output_file = h_output_file if h_output_file is not None else None
+
+        # time
+        self.sample_time = 0.00
+        self.current_time = time.time()
+        self.last_time = self.current_time
+        
+
+        self.pwm = pwm
+        
+        # sampling rate: 100Hz (100次/1秒)
+        self.samplingRate = 100
+
+        self.timestamp_pone = 0
+
+        self.board = Arduino(PORT)
+
+        # arduino pwm pin, 3 5 6 9 10 11
+        self.pwm_pin = 3
+        self.board.digital[self.pwm_pin].mode = PWM
+        
+        # read pin, 0
+        self.pd_pin = 0
+
+        # reset pins
+        self.pinOut(self.pwm_pin, 0.0)
+
+
+    def start(self):
+        # setting sampling for temperature
+        self.pinOut(self.pwm_pin, self.pwm)
+        self.board.analog[self.pd_pin].register_callback(self.PDreadCallback)
+        self.board.samplingOn(1000 / self.samplingRate)
+        self.board.analog[self.pd_pin].enable_reporting()
+
+    def PDreadCallback(self, data):
+        data = self.calT(data)
+        print("Timestamp: %f, PD_READER: %f" % (self.timestamp_pone, data))
+        self.timestamp_pone += (1 / self.samplingRate)
+    
+    def stop(self):
+        self.board.samplingOff()
+        self.pinOut(self.pwm_pin, 0.0)
+        self.board.exit()
+        
+    def calT(self, data):
+        ret = 5.0*data
+        return ret
+
+    # pin output
+    def pinOut(self, pin, value=1.0):
+        print("set digital[%d] value: %f" % (pin, value))
+        self.board.digital[pin].write(value)
+
+
+
+try: 
+
+
+    print("Let's print data from Arduino's analog pins for N secs.")
+    # Let's create an instance
+    value = args.voltage / 5.0
+
+    if args.voltage < 0.0:
+        value = 0
+    if args.voltage >= 5.0:
+        value = 1.0
+
+    pd_sensor = PDReader(pwm=value)
+    # and start DAQ
+    pd_sensor.start()
+    # let's acquire data for 100secs. We could do something else but we just sleep!
+    time.sleep(args.time)
+    # let's stop it
+    pd_sensor.stop()
+    print("finished")
+
+except Exception as e:
+    print(e)
+
