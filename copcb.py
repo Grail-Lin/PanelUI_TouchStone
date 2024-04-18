bd4c8aa4dff4d67afb43a4cc23637740bade6df8
Grail Lin <grail.lin@gmail.com>
2024-04-19 05:08:20 +0800

    Update copcb.py

--- copcb.py
+++ copcb.py
@@ -247,8 +247,11 @@ class ModuleBT(COPcbConnector):
         # send start cmd
         print("CMD String: %s" % str(cmd_str))
         self.ser.write(cmd_str)
+        cmd_array = cmd_str.decode("utf-8").strip().split(',')
+
         time.sleep(0.1)
         nowtime = time.time()
+
 
         '''
             loop until return == 1
@@ -277,8 +280,9 @@ class ModuleBT(COPcbConnector):
                         ret_array = ret_string.strip().split(',')
 
                         if len(ret_array) == 4:
-                            if ret_array[3] != '0':
-                                needReturn = 1
+                            if ret_array[0] == cmd_array[0] and ret_array[1] == cmd_array[1]:
+                                if ret_array[3] != '0':
+                                    needReturn = 1
                         else:
                             needReturn = 1
                         
@@ -335,7 +339,7 @@ class ModuleBT(COPcbConnector):
     # 1: cartridge rotation
     def rotateCart(self, timeout = 10, pos = 0):
         if pos == 0:
-            ret = self.sendCmd(timeout, b'1,1,10000,0\n')
+            ret = self.sendCmd(timeout, b'1,1,%d,0\n' % timeout*1000)
             print("Rotate Cartridge to zero position....")
         else:
             ret = self.sendCmd(timeout, b'1,2,%d,0\n' % pos)
@@ -345,10 +349,10 @@ class ModuleBT(COPcbConnector):
     # 2: cup driver
     def moveCDriver(self, timeout = 3, back = True):
         if back == True:
-            ret = self.sendCmd(timeout, b'2,1,3000,0\n')
+            ret = self.sendCmd(timeout, b'2,1,%d,0\n' % timeout*1000)
             print("Move cup driver backward....")
         else:
-            ret = self.sendCmd(timeout, b'2,2,3000,0\n')
+            ret = self.sendCmd(timeout, b'2,2,%d,0\n' % timeout*1000)
             print("Move cup driver forward....")
         return self.checkOK(ret)
 
@@ -356,20 +360,20 @@ class ModuleBT(COPcbConnector):
     # 3: rocker arm
     def moveRArm(self, timeout = 5, release = True):
         if release == True:
-            ret = self.sendCmd(timeout, b'3,1,2000,0\n')
+            ret = self.sendCmd(timeout, b'3,1,%d,0\n' % timeout*1000)
             print("Release rocker arm....")
         else:
-            ret = self.sendCmd(timeout, b'3,2,2000,0\n')
+            ret = self.sendCmd(timeout, b'3,2,%d,0\n' % timeout*1000)
             print("Contact rocker arm....")
         return self.checkOK(ret)
 
     # 4: optical position
     def moveOPos(self, timeout = 5, up = True):
         if up == True:
-            ret = self.sendCmd(timeout, b'4,1,2000,0\n')
+            ret = self.sendCmd(timeout, b'4,1,%d,0\n' % timeout*1000)
             print("Move optical position up....")
         else:
-            ret = self.sendCmd(timeout, b'4,2,2000,0\n')
+            ret = self.sendCmd(timeout, b'4,2,%d,0\n' % timeout*1000)
             print("Move optical position down....")
         return self.checkOK(ret)
 
@@ -377,36 +381,36 @@ class ModuleBT(COPcbConnector):
     # 5: reverses motor
     # 6: door
     def openDoor(self, timeout = 1):
-        ret = self.sendCmd(timeout, b'6,1,1000,0\n')
+        ret = self.sendCmd(timeout, b'6,1,%d,0\n' % timeout*1000)
         print("Open door....")
         return self.checkOK(ret)
     def closeDoor(self, timeout = 1):
-        ret = self.sendCmd(timeout, b'6,2,1000,0\n')
+        ret = self.sendCmd(timeout, b'6,2,%d,0\n' % timeout*1000)
         print("Close door....")
         return self.checkOK(ret)
 
     # 7: Cartridge Roller
     def moveCartRoller(self, timeout = 6, back = True):
         if back == True:
-            ret = self.sendCmd(timeout, b'7,1,6000,0\n')
+            ret = self.sendCmd(timeout, b'7,1,%d,0\n' % timeout*1000)
             print("Move cartridge roller backward....")
         else:
-            ret = self.sendCmd(timeout, b'7,2,6000,0\n')
+            ret = self.sendCmd(timeout, b'7,2,%d,0\n' % timeout*1000)
             print("Move cartridge roller forward....")
         return self.checkOK(ret)
 
 
     # 8: vertical position
     def moveVertPosTop(self, timeout = 36):
-        ret = self.sendCmd(timeout, b'8,1,36000,0\n')
+        ret = self.sendCmd(timeout, b'8,1,%d,0\n' % timeout*1000)
         print("move vertical position to TOP")
         return self.checkOK(ret)
     def moveVertPosMid(self, timeout = 18):
-        ret = self.sendCmd(timeout, b'8,2,18000,0\n')
+        ret = self.sendCmd(timeout, b'8,2,%d,0\n' % timeout*1000)
         print("move vertical position to MID")
         return self.checkOK(ret)
     def moveVertPosBtm(self, timeout = 36):
-        ret = self.sendCmd(timeout, b'8,3,36000,0\n')
+        ret = self.sendCmd(timeout, b'8,3,%d,0\n' % timeout*1000)
         print("move vertical position to BTM")
         return self.checkOK(ret)
 
@@ -414,11 +418,11 @@ class ModuleBT(COPcbConnector):
     def startBLDCMotor(self, timeout = 5, clockwise = True):
         if clockwise == True:
             # clockwise
-            ret = self.sendCmd(timeout, b'9,1,5000,0\n')
+            ret = self.sendCmd(timeout, b'9,1,%d,0\n' % timeout*1000)
             print("turn on BLDC Motor: clockwise....")
         else:
             # counter clockwise
-            ret = self.sendCmd(timeout, b'9,2,5000,0\n')
+            ret = self.sendCmd(timeout, b'9,2,%d,0\n' % timeout*1000)
             print("turn on BLDC Motor: counter clockwise....")
         return self.checkOK(ret)
 
@@ -594,7 +598,7 @@ class ModuleBT(COPcbConnector):
 
     # 20 combo cmd
     def insertCart(self, timeout = 60):
-        ret = self.sendCmd(timeout, b'20,2,60000,0\n')
+        ret = self.sendCmd(timeout, b'20,2,%d,0\n' % timeout*1000)
         if self.checkOK(ret) == True:
             return True
         else:
@@ -603,7 +607,7 @@ class ModuleBT(COPcbConnector):
             return ret
 
     def ejectCart(self, timeout = 60):
-        ret = self.sendCmd(timeout, b'20,1,60000,0\n')
+        ret = self.sendCmd(timeout, b'20,1,%d,0\n' % timeout*1000)
         return self.checkOK(ret)
 
     def forceCloseCart(self, timeout = 4):
