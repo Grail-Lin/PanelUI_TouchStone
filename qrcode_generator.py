25d9a010effbcb61124a2e71ea93e46d6d4c8606
Grail-Lin <grail.lin@gmail.com>
2024-12-23 02:49:06 +0800

    Create qrcode_generator.py

---
+++ qrcode_generator.py
@@ -0,0 +1,42 @@
+# Importing library
+import sys
+import qrcode
+from PIL import Image
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('no argument')
+        sys.exit()
+
+
+
+    # Data to encode
+    #data = "GeeksforGeeks11GeeksforGeeks12GeeksforGeeks13GeeksforGeeks14GeeksforGeeks15GeeksforGeeks16GeeksforGeeks17GeeksforGeeks18GeeksforGeeks19GeeksforGeeks20GeeksforGeeks21GeeksforGeeks22GeeksforGeeks23"
+    data = sys.argv[1]
+    print("QRcode Data String = %s" % data)
+
+    filename = "default_QRcode.png"
+    filename2 = "default_QRcode_54x54.png"
+    if len(sys.argv) > 2:
+        filename = sys.argv[2] + ".png"
+        filename2 = sys.argv[2] + "_54x54.png"
+
+    # Creating an instance of QRCode class
+    qr = qrcode.QRCode(version = 1,
+                       box_size = 2,
+                       border = 1)
+
+    # Adding data to the instance 'qr'
+    qr.add_data(data)
+
+    qr.make(fit = True)
+    img = qr.make_image(fill_color = 'black',
+                        back_color = 'white')
+
+    img.save(filename)
+
+    resize = (54, 54)
+    img = img.resize(resize)
+    img.save(filename2)
+
